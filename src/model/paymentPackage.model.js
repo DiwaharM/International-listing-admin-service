a3270e3c1b5d2f4f1c534a22c77b78654a121a5d
Diwahar <ripsildeveloper4@gmail.com>
2019-06-08 12:41:48 +0530

    add in paymentPackage

--- src/model/paymentPackage.model.js
+++ src/model/paymentPackage.model.js
@@ -4,8 +4,6 @@ const PaymentSchema = new mongoose.Schema({
     duration: Number,
     amount: Number,
     grade: Number,
-    startingDate: Date,
-    closingDate: Date,
     description: String
 });
 const Payment = mongoose.model('paymentPackage', PaymentSchema);
